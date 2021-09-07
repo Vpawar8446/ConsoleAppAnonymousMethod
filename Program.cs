52428927e9725d4abf741231f0c8cc31bd206fd1
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 00:14:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppAnonymousMethod
+{ 
+    class Program
+    {
+        public delegate void DelegateSquare(int n);
+
+        static void Main(string[] args)
+        {
+            DelegateSquare ds = delegate (int x)
+            {
+                Console.WriteLine(x * x);
+            };
+
+            ds(4);
+
+        }
+    }
+}
